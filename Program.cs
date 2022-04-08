f71e329a2f0093297e7b26d955870c6aa6b2c565
Nevillested <olegsidnev@gmail.com>
2022-04-08 22:18:07 +0300

    hzhz

--- Program.cs
+++ Program.cs
@@ -14,17 +14,8 @@ var body = await response_sec.Content.ReadAsStringAsync();
 
 dynamic weather = JsonConvert.DeserializeObject<dynamic>(body);
 
+Console.WriteLine("Learn english mthrfckr");
 foreach (var day in weather.days)
 {
-    string weather_date = day.datetime;
-    string weather_desc = day.description;
-    string weather_tmax = day.tempmax;
-    string weather_tmin = day.tempmin;
-
-    Console.WriteLine("Learn english mthrfckr");
-    Console.WriteLine("Forecast for date: " + weather_date);
-    Console.WriteLine("Forecast for date: " + weather_date);
-    Console.WriteLine("General conditions: " + weather_desc);
-    Console.WriteLine("The high temperature will be " + weather_tmax);
-    Console.WriteLine("The low temperature will be: " + weather_tmin);
+    Console.WriteLine("Date: " + day.datetime + ". Desccription: " + day.description + " Max t:" + day.tempmax + "°. Min t:" + day.tempmin+ "°.");
 }
